73cd262e268bd41e9fc6042ac384670d6f74f657
Jeevan Thapa <jeevanthapa1987@yahoo.com>
2021-12-30 09:40:06 -0500

    add the task form

--- this.py
+++ this.py
@@ -1,19 +1,21 @@
-var buttonEl = document.querySelector("#save-task");
+console.log(window.document);
+console.dir(window.document.querySelector("button"));
+
+var btn = window.document.querySelector("button").textContent;
+console.dir(btn);
+
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 
-console.log(buttonEl.addEventListener("click", function() {
-    alert("button clicked");
-    var taskItemEl = document.createElement("li");
-    taskItemEl.textContent = "hello";
-taskItemEl;
-tasksToDoEl.appendChild(taskItemEl);
-taskItemEl.className = "task-item";
-  }));
+var buttonEl = document.querySelector("#save-task");
 
-  buttonEl.addEventListener("click", function() {
-  var listItemEl = document.createElement("li");
-  listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
-  tasksToDoEl.appendChild(listItemEl);
-});
+console.log(buttonEl.addEventListener("click", function() {
+  var taskItemEl = document.createElement("li");
+  taskItemEl.textContent = "hello";
+  tasksToDoEl.appendChild(taskItemEl);
+  taskItemEl.className = "task-item";
+  taskItemEl;
+  alert("button clicked");
+}));
+
+
